09b6e82462ebe3cacd3f917a5938cd1b0f08cbc3
rushen <realrushen@gmail.com>
2020-11-03 22:00:30 +0300

    added permissions for model Book, now only owner or staff can edit\delete this data

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BookSerializer
 
 
@@ -11,9 +12,14 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_fields = ['price', 'author_name']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
 
 def auth(request):
