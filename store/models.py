e0fa55bcece8e015b99a9f9c7089f1620d190222
rushen <realrushen@gmail.com>
2020-11-05 20:54:55 +0300

    added new model UserBookRelation for rating and likes functionality

--- store/models.py
+++ store/models.py
@@ -6,7 +6,30 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
 
     def __str__(self):
-        return f'ID {self.id}: {self.name}'
+        return f'ID {self.id}: {self.name} owner: {self.owner.username}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ok'),
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Amazing'),
+        (5, 'Incredible'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
+
+    def __str__(self):
+        return f'{self.user.username}-{self.book.name}, ' \
+               f'like: {self.like}, ' \
+               f'in_bookmarks: {self.in_bookmarks}, ' \
+               f'rate: {self.rate}'
