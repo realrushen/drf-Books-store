aff3b9a72c193d7e00d9b41e126d3704cd6a4631
rushen <realrushen@gmail.com>
2020-11-05 22:14:10 +0300

    serializers edited to match new rating and likes functionality

--- store/serializers.py
+++ store/serializers.py
@@ -1,10 +1,22 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 
 
 class BookSerializer(ModelSerializer):
+    annotated_likes = serializers.IntegerField(read_only=True)
+    rating = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author_name', 'owner', 'annotated_likes', 'rating',)
 
+    def get_likes_count(self, instance):
+        return UserBookRelation.objects.filter(book=instance, like=True).count()
+
+
+class UserBookRelationSerializer(ModelSerializer):
+    class Meta:
+        model = UserBookRelation
+        fields = ('book', 'like', 'in_bookmarks', 'rate')
