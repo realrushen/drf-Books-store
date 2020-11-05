4acee4f1bb91f35d58159d2c1fc57d76657bc9b7
rushen <realrushen@gmail.com>
2020-11-05 22:08:46 +0300

    UserBookRelation model added to admin panel, view and test added  for rating and likes functionality, new view registered in urls.py

--- store/views.py
+++ store/views.py
@@ -1,15 +1,21 @@
+from django.db.models import Count, Case, When, Avg
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 from store.permissions import IsOwnerOrStaffOrReadOnly
-from store.serializers import BookSerializer
+from store.serializers import BookSerializer, UserBookRelationSerializer
 
 
 class BookViewSet(ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().annotate(
+        annotated_likes=Count(Case(When(userbookrelation__like=True, then=1))),
+        rating=Avg('userbookrelation__rate')
+    )
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     permission_classes = [IsOwnerOrStaffOrReadOnly]
@@ -24,3 +30,14 @@ class BookViewSet(ModelViewSet):
 
 def auth(request):
     return render(request, 'oauth.html')
+
+
+class UserBookRelationView(UpdateModelMixin, GenericViewSet):
+    queryset = UserBookRelation.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserBookRelationSerializer
+    lookup_field = 'book'
+
+    def get_object(self):
+        obj, _ = UserBookRelation.objects.get_or_create(user=self.request.user, book_id=self.kwargs['book'])
+        return obj
